82afdb8dd906ab2044e2b8a790bb775792b40482
MadhuMitha032004 <madhumitha03155@gmail.com>
2024-08-01 15:24:36 +0530

    Time: 20 ms (79.2%), Space: 96 MB (9.42%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        for (auto n : nums2) {
+            nums1.push_back(n);
+        }
+        sort(nums1.begin(), nums1.end());
+        int size1 = nums1.size();
+        int t = size1;
+        if (t % 2 == 0) {
+            return float(float(nums1[(t/2)-1]+nums1[t/2])/2);
+        } 
+       
+        else { t=t/2;
+        float ans=float(nums1[t]);
+            return ans;           
+            
+        }
+    }
+};
